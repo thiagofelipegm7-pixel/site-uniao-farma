f99c2bf080028d457dbb1d60c24e34d80beeef81
thiagofelipegm7-pixel <thiagofelipegm7@gmail.com>
2026-09-04 16:21:11 -0300

    visual: mobile button system with 48px targets and clear primary

--- app/MobileQuickNav.tsx
+++ app/MobileQuickNav.tsx
@@ -1,9 +1,15 @@
+import { IntentIcon } from "./IntentIcons";
+import "./mobile-buttons.css";
+
 export default function MobileQuickNav() {
   return (
     <nav className="mobile-quick-nav" aria-label="Atalhos do site">
       <a href="/ofertas">Ofertas</a>
-      <a href="/#unidades-rapidas">Unidades</a>
-      <a href="/receita">Receita</a>
+      <a className="is-primary" href="/#unidades-rapidas">Unidades</a>
+      <a href="/receita">
+        <IntentIcon intent="recipe" />
+        Receita
+      </a>
     </nav>
   );
 }
