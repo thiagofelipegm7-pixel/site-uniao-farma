78ceb7e08f617b97a2d00947b5c53bc57219d33f
thiagofelipegm7-pixel <thiagofelipegm7@gmail.com>
2026-09-04 17:37:25 -0300

    visual: load Family + ElevenLabs + Apple mix

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,7 @@ import "./visual-detail.css";
 import "./type.css";
 import "./viewport.css";
 import "./motion.css";
+import "./refero.css";
 import { INSTAGRAM_URL, SITE_URL, UNITS } from "./site-config";
 
 const AnalyticsConsent = dynamic(() => import("./AnalyticsConsent"), { ssr: false });
