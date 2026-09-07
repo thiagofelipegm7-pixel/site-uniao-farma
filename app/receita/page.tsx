46dbceedfa28ebc6daaf6319cebd82bec47ba80b
thiagofelipegm7-pixel <thiagofelipegm7@gmail.com>
2026-09-07 12:32:20 -0300

    chore(cache): /receita ilustração ?v=5

--- app/receita/page.tsx
+++ app/receita/page.tsx
@@ -28,7 +28,7 @@ export default function ReceitaPage() {
             </div>
             <img
               className="recipe-illustration"
-              src="/illustrations/atendimento.svg?v=4"
+              src="/illustrations/atendimento.svg?v=5"
               alt="Farmacêutica orientando uma cliente no balcão da União Farma"
               width="1200"
               height="900"
