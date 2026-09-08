759a0af29bb5c3b459ec7357d6b242095730e3ff
thiagofelipegm7-pixel <thiagofelipegm7@gmail.com>
2026-09-07 21:21:40 -0300

    Adiciona fonte da foto do interior da Fátima

---
+++ app/fatima-interior-src.ts
@@ -0,0 +1,5 @@
+import { P1 } from "./fatima-interior-p1";
+import { P2 } from "./fatima-interior-p2";
+import { P3 } from "./fatima-interior-p3";
+
+export const FATIMA_INTERIOR_SRC = "data:image/jpeg;base64," + P1 + P2 + P3;
