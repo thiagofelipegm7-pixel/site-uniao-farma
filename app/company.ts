ae18247b8eaa8d61c551bab906156d81ed617c9c
thiagofelipegm7-pixel <thiagofelipegm7@gmail.com>
2026-09-09 12:13:42 -0300

    Exibe CNPJ no rodape e forca fotos de oferta e novidade.

---
+++ app/company.ts
@@ -0,0 +1,6 @@
+export const COMPANY = {
+  legalName: "Drogaria e Perfumaria União Farma Ltda",
+  cnpj: "28.455.556/0001-91",
+  pharmacistNote:
+    "Farmacêutico responsável presente no horário de funcionamento. O CRF de cada unidade fica no balcão da loja.",
+};
